85c08cb25f9d72dae57fc0e1aea2958092df767e
Emil <eetaller@gmail.com>
2023-11-06 03:00:45 +0100

    Add Pizza model

--- apps/pizzaplace/models.py
+++ apps/pizzaplace/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(upload_to='pizza_images', blank=True)
+
+    def __str__(self):
+        return self.name
