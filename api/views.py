8fe52274cbeca15fc5a782d6e7d0837958cbb10a
Emil <eetaller@gmail.com>
2023-11-07 01:39:07 +0100

    Add links to api root

--- api/views.py
+++ api/views.py
@@ -4,24 +4,29 @@ from django.urls import get_resolver
 
 
 class APIRoot(APIView):
-    def get_url_patterns(self, url_list, resolver, prefix=''):
+
+    def get_url_patterns(self, url_dict, resolver, prefix=''):
         """
-        Recursively build a list of all URL patterns in the resolver tree.
+        Build a dicitonary of all URL patterns.
         """
+
+        current_url = self.request.build_absolute_uri('/') # Get the current URL
+        
         for pattern in resolver.url_patterns:
             if hasattr(pattern, 'url_patterns'):
-                self.get_url_patterns(url_list, pattern, prefix + pattern.pattern.regex.pattern)
+                self.get_url_patterns(url_dict, pattern, prefix + pattern.pattern.regex.pattern)
             else:
                 url = prefix + pattern.pattern.regex.pattern
                 url = url.replace('^', '').replace('\\Z', '')
                 if not url.startswith('api-auth') and not url.startswith('admin'):
-                    url_list.append(url)
+                    url_dict[url] = current_url + url
 
     def get(self, request, format=None):
         """
-        Return a list of all endpoints in the API.
+        Return a dictionary of all endpoints in the API.
         """
-        url_list = []
+        url_dict = {}
         resolver = get_resolver()
-        self.get_url_patterns(url_list, resolver)
-        return Response(url_list)
+        self.get_url_patterns(url_dict, resolver)
+        return Response(url_dict)
+    
